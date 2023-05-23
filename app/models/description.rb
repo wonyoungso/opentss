04c965124d53fa2ba3ab18ce0a3a456076f330ba
Wonyoung So <wso@mit.edu>
2023-05-23 19:56:46 -0400

    update

--- app/models/description.rb
+++ app/models/description.rb
@@ -38,4 +38,26 @@ class Description < ApplicationRecord
         
   end
 
+  # After active storage urls are changed, use this to recreate all trix attachments
+  def self.refresh_trixes
+    ActionText::RichText.where.not(body: nil).find_each do |trix|
+      Description.refresh_trix(trix)
+    end
+  end
+
+  # After active storage urls are changed, use this to recreate a specific trix attachments
+  def self.refresh_trix(trix)
+    return unless trix.embeds.size.positive?
+
+    trix.body.fragment.find_all("action-text-attachment").each do |node|
+      embed = trix.embeds.find { |attachment| attachment.filename.to_s == node["filename"] && attachment.byte_size.to_s == node["filesize"] }
+
+      node.attributes["url"].value = Rails.application.routes.url_helpers.rails_storage_redirect_url(embed.blob, host: "open-tss.net")
+      node.attributes["sgid"].value = embed.attachable_sgid
+    end
+
+    trix.update_column :body, trix.body.to_s
+  end
+
+
 end
