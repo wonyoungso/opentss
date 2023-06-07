8be2bed7ef8263a0ceddcfe0036c72e171ec5029
Wonyoung So <wso@mit.edu>
2023-06-07 15:30:00 -0400

    tremendous production update

--- config/initializers/tremendous.rb
+++ config/initializers/tremendous.rb
@@ -1,3 +1,11 @@
 require 'tremendous'
-url = "https://testflight.tremendous.com/api/v2/"
-TREMENDOUS_LIBRARY = Tremendous::Rest.new(Rails.application.credentials.dig(:tremendous, :sandbox_access_token), url) 
+if rails.env.development?
+  access_token = Rails.application.credentials.dig(:tremendous, :sandbox_access_token)
+  api_url = Rails.application.credentials.dig(:tremendous, :sandbox_api_url)
+else
+  access_token = Rails.application.credentials.dig(:tremendous, :access_token)
+  api_url = Rails.application.credentials.dig(:tremendous, :api_url)
+end
+
+
+TREMENDOUS_LIBRARY = Tremendous::Rest.new(access_token, api_url)
